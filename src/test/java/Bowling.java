03f630c56abfc93cc12998284f89230fb91dc456
lemon-piepie <purplechilde@163.com>
2020-08-03 13:04:22 +0800

    git score when strike.

--- src/test/java/Bowling.java
+++ src/test/java/Bowling.java
@@ -1,7 +1,9 @@
 public class Bowling {
     int throwtwice(int[] throwhit){
         if ((throwhit[0]+throwhit[1]) == 10){
-            return (10+throwhit[3]+throwhit[4]);
+            return (10+throwhit[2]+throwhit[3]);
+        }else if ((throwhit[0]+throwhit[1]) == 10 && throwhit[1] == 0){
+            return (10+throwhit[2]+throwhit[3]+throwhit[4]+throwhit[5]);
         }
         return (throwhit[0]+throwhit[1]);
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -12,9 +12,9 @@ public class BowlingGameTest {
         Bowling bowl = new Bowling();
         int[] throwhit=new int[]{2,5};
         //When
-        int socre = bowl.throwtwice(throwhit);
+        int score = bowl.throwtwice(throwhit);
         //Then
-        assertEquals(7, socre);
+        assertEquals(7, score);
     }
 
     @Test
@@ -23,8 +23,19 @@ public class BowlingGameTest {
         Bowling bowl = new Bowling();
         int[] throwhit=new int[]{8,2,3,6};
         //When
-        int socre = bowl.throwtwice(throwhit);
+        int score = bowl.throwtwice(throwhit);
         //Then
-        assertEquals(19, socre);
+        assertEquals(19, score);
+    }
+
+    @Test
+    void should_get_10_and_next_two_frames_score_if_strike(){
+        //Given
+        Bowling bowl = new Bowling();
+        int[] throwhit = new int[]{10,0,8,1,5,2};
+        //When
+        int score = bowl.throwtwice(throwhit);
+        //Then
+        assertEquals(26, score);
     }
 }
